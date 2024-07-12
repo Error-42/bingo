5eaa5a245454b2594602f6250ac9da779c0a4ead
Error-42 <erroruplus002d42@gmail.com>
2024-07-12 20:03:13 +0200

    first commit

---
+++ generation.js
@@ -0,0 +1,43 @@
+function start() {
+    fetch("bingo.txt")
+        .then(res => res.text())
+        .then(text => populateTables(text));
+}
+
+function populateTables(rawText) {
+    let texts = parseText(rawText);
+
+    let searchParams = new URLSearchParams(window.location.search);
+
+    let width = parseInt(searchParams.get("width"));
+    let height = parseInt(searchParams.get("height"));
+
+    let table = document.getElementById("main");
+
+    for (let rowIndex = 0; rowIndex < height; rowIndex++) {
+        let row = table.insertRow();
+
+        for (let cellIndex = 0; cellIndex < width; cellIndex++) {
+            let cell = row.insertCell();
+
+            const textIndex = rowIndex * width + cellIndex;
+
+            if (textIndex >= texts.length) {
+                // Maybe provide a more helpful error message?
+                continue;
+            }
+            
+            cell.innerHTML += `<div class="long">${texts[textIndex].short}</div>`;
+            cell.innerHTML += `<div class="short">${texts[textIndex].long}</div>`;
+        }
+    }
+}
+
+function parseText(text) {
+    return text.split("\n")
+        .map(line => {
+            [short, long] = line.split("--").map(part => part.trim());
+
+            return { short, long };
+        });
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+function go() {
+    let height = parseInt(document.getElementById("height").value);
+    let width = parseInt(document.getElementById("width").value);
+
+    window.location.href = `generation.html?width=${width}&height=${height}`;
+}
